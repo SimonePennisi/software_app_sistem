809dbfd26b6319d191a00e12d929fe294c143a05
wolfboy <59877082+SimonePennisi@users.noreply.github.com>
2021-12-23 20:39:43 +0000

    collegamento a siti non nello script ancora da rifinire

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import webbrowser as wb
 import time
 
 while True:
-  app_aprire = str(input("Cosa vuoi fare? "))
+  azione = str(input("Cosa vuoi fare? "))
 
-  if "app" in app_aprire:
-    if "youtube" in app_aprire:
+  if "app" in azione:
+    if "youtube" in azione:
       wb.open("https://www.youtube.com/")
-    elif "instagram" in app_aprire:
+    elif "instagram" in azione:
       wb.open("https://www.instagram.com/")
-    elif "facebook" in app_aprire:
+    elif "facebook" in azione:
       wb.open("https://www.facebook.com/")
-    elif "whatsapp" in app_aprire:
+    elif "whatsapp" in azione:
       wb.open("https://web.whatsapp.com/")
-  elif "hour" in app_aprire:
+    else:
+      sito = str(input("Non ho capito il sito, potresti ridirmelo:"))
+      sitoURL_generator = (f"https://www.{sito}.com/")
+      wb.open(sitoURL_generator)
+
+  elif "hour" in azione:
     orario = time.strftime("%H.%M")
     print(orario)
   else:
